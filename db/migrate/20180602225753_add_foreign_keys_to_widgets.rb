cc8cc4a87923767f282c3a31986edb9cd95d1d36
Daniel Wheeler <dwheeler@customink.com>
2018-06-02 23:24:53 -0400

    spelling error in migrations

--- db/migrate/20180602225753_add_foreign_keys_to_widgets.rb
+++ db/migrate/20180602225753_add_foreign_keys_to_widgets.rb
@@ -1,6 +1,6 @@
 class AddForeignKeysToWidgets < ActiveRecord::Migration[5.2]
   def change
     add_reference :widgets, :size, foreign_key: true
-    add_reference :widgets, :colors, foreign_key: true
+    add_reference :widgets, :color, foreign_key: true
   end
 end
--- db/migrate/20180602232150_change_foreign_key_names.rb
+++
@@ -1,6 +0,0 @@
-class ChangeForeignKeyNames < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :widgets, :sizes_id, :size_id
-    rename_column :widgets, :colors_id, :color_id
-  end
-end
