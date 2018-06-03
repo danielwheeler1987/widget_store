0c7dfa15c52e0fecd60524225c99413cb01251ec
Daniel Wheeler <dwheeler@customink.com>
2018-06-02 22:49:46 -0400

    Getting the initial internal widget tool working

---
+++ app/controllers/widgets_controller.rb
@@ -0,0 +1,50 @@
+class WidgetsController < ApplicationController
+  before_action :set_widget, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @widgets = Widget.order(:name).page params[:page]
+  end
+
+  def show
+  end
+
+  def new
+    @widget = Widget.new
+  end
+
+  def edit
+  end
+
+  def create
+    @widget = Widget.new(widget_params)
+
+    if @widget.save
+      redirect_to widgets_url, notice: 'Great! Widget was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @widget.update(widget_params)
+      redirect_to widgets_url, notice: 'Great! Widget was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @widget.destroy
+    redirect_to widgets_url, notice: 'Great! Widget was successfully destroyed.'
+  end
+
+  private
+
+  def set_widget
+    @widget = Widget.find(params[:id])
+  end
+
+  def widget_params
+    params.fetch(:widget, {}).permit(:name, :quantity, :color_id, :size_id)
+  end
+end
--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,2 +1,9 @@
 class Widget < ApplicationRecord
+  paginates_per 10
+  
+  validates :name, :color_id, :size_id, :quantity, presence: true
+  validates_numericality_of :quantity, greater_than: 0
+
+  belongs_to :color
+  belongs_to :size
 end
